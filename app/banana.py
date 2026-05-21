702952900bca3033ca5fcfd4003152f47a48c9b2
Karina Tlep <carrietlep@yandex.com>
2026-05-21 11:13:47 +0500

    i added sign-up\home pages

--- app/banana.py
+++ app/banana.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
+# SIGN-UP / LOGIN PAGE
 @app.route("/")
+def signup():
+    return render_template("sign-up.html")
+
+
+# HOME PAGE
+@app.route("/home")
 def home():
-    return "Monkey project is alive 🐒"
+    return render_template("home.html")
+
+
+# LOGOUT ROUTE
+@app.route("/logout")
+def logout():
+    return redirect(url_for("signup"))
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
